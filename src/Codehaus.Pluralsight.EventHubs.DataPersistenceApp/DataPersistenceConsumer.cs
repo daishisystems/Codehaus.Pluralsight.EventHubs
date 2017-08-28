0fbad8332989d2fd30660035552f9ba2e4d81a74
Paul Mooney <paul@daishisystems.com>
2017-08-28 13:52:07 +0100

    Added data-persistence application. Renamed real-time analysis application.

---
+++ src/Codehaus.Pluralsight.EventHubs.DataPersistenceApp/DataPersistenceConsumer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+
+namespace Codehaus.Pluralsight.EventHubs.DataPersistenceApp
+{
+    internal class DataPersistenceConsumer : IEventProcessor
+    {
+        private Stopwatch _checkpointStopWatch;
+
+        async Task IEventProcessor.CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Releasing lease on Partition {0}. Reason: {1}.", context.Lease.PartitionId, reason);
+            if (reason == CloseReason.Shutdown)
+                await context.CheckpointAsync();
+            Console.ReadLine();
+        }
+
+        Task IEventProcessor.OpenAsync(PartitionContext context)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("I am listening to Partition {0}, at position {1}.",
+                context.Lease.PartitionId, context.Lease.Offset);
+            _checkpointStopWatch = new Stopwatch();
+            _checkpointStopWatch.Start();
+            return Task.FromResult<object>(null);
+        }
+
+        async Task IEventProcessor.ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (var eventData in messages)
+            {
+                var data = Encoding.UTF8.GetString(eventData.GetBytes());
+                var deviceTelemetry = JsonConvert.DeserializeObject<DeviceTelemetry>(data);
+
+                const string connectionstring =
+                    "Server=tcp:mooney.database.windows.net,1433;Initial Catalog=mooney;Persist Security Info=False;User ID=mooney;Password=M3c54n1c4L;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+                using (var connection = new SqlConnection(connectionstring))
+                {
+                    connection.Open();
+
+                    const string sqlCommandText =
+                        "insert into DeviceTelemetry(IPAddress,Time,DeviceType,IsOn) values (@IPAddress,@Time,@DeviceType,@IsOn);";
+                    using (var command = new SqlCommand(sqlCommandText, connection))
+                    {
+                        command.Parameters.AddWithValue("@IPAddress", deviceTelemetry.IpAddress);
+                        command.Parameters.AddWithValue("@Time", deviceTelemetry.Time);
+                        command.Parameters.AddWithValue("@DeviceType", deviceTelemetry.DeviceType);
+                        command.Parameters.AddWithValue("@IsOn", deviceTelemetry.IsOn);
+
+                        Console.WriteLine($"Added Device IP {deviceTelemetry.IpAddress} to the database.");
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            if (_checkpointStopWatch.Elapsed > TimeSpan.FromMinutes(5))
+            {
+                await context.CheckpointAsync();
+                _checkpointStopWatch.Restart();
+            }
+        }
+    }
+}
---
+++ src/Codehaus.Pluralsight.EventHubs.DataPersistenceApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Codehaus.Pluralsight.EventHubs.DataPersistenceApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string eventHubConnectionString =
+                "Endpoint=sb://mooney.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SATW/1oIIF9dAAPbgF3YLkDjMmGsYMvs/Fi+Vutz8bo=";
+            const string eventHubName = "myeventhub";
+            const string storageAccountName = "mooneyeventhub";
+            const string storageAccountKey =
+                "f++Lw/zix9ryWhgRRMqyRqr4QP4Np17BT21IbEzyEGH3SK7gT1byIdUPW7iVHau9xF8fTtANtC5c4ky+RUKJCw==";
+            var storageConnectionString =
+                $"DefaultEndpointsProtocol=https;AccountName={storageAccountName};AccountKey={storageAccountKey}";
+
+            var eventProcessorHostName = Guid.NewGuid().ToString();
+            var eventProcessorHost = new EventProcessorHost(
+                eventProcessorHostName,
+                eventHubName,
+                "datapersistence",
+                eventHubConnectionString,
+                storageConnectionString);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Registering with Event Hubs...");
+            var options = new EventProcessorOptions();
+            options.ExceptionReceived += (sender, e) => { Console.WriteLine(e.Exception); };
+            eventProcessorHost.RegisterEventProcessorAsync<DataPersistenceConsumer>(options).Wait();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker...");
+            Console.ReadLine();
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+    }
+}
