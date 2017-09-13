90b0837fb2e8a5fe2fb3495abe77cd1f8a95b816
Paul Mooney <paul@daishisystems.com>
2017-09-13 18:19:09 +0100

    Applied original Publisher code (very simple).

--- src/Codehaus.Pluralsight.EventHubs.ConsoleApp/Program.cs
+++ src/Codehaus.Pluralsight.EventHubs.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.ServiceBus.Messaging;
 
 namespace Codehaus.Pluralsight.EventHubs.ConsoleApp
@@ -7,11 +8,11 @@ namespace Codehaus.Pluralsight.EventHubs.ConsoleApp
     {
         private static void Main(string[] args)
         {
-            var eventHubConnectionString =
+            const string eventHubConnectionString =
                 "Endpoint=sb://mooney.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SATW/1oIIF9dAAPbgF3YLkDjMmGsYMvs/Fi+Vutz8bo=";
-            var eventHubName = "myeventhub";
-            var storageAccountName = "mooneyeventhub";
-            var storageAccountKey =
+            const string eventHubName = "myeventhub";
+            const string storageAccountName = "mooneyeventhub";
+            const string storageAccountKey =
                 "f++Lw/zix9ryWhgRRMqyRqr4QP4Np17BT21IbEzyEGH3SK7gT1byIdUPW7iVHau9xF8fTtANtC5c4ky+RUKJCw==";
             var storageConnectionString =
                 $"DefaultEndpointsProtocol=https;AccountName={storageAccountName};AccountKey={storageAccountKey}";
@@ -21,34 +22,8 @@ namespace Codehaus.Pluralsight.EventHubs.ConsoleApp
             publisher.Init(
                 "Endpoint=sb://mooney.servicebus.windows.net/;SharedAccessKeyName=Publish;SharedAccessKey=YF3Eu+QUmfuxBXhlWvibaKRPVfgKvGr9eo25FId3nIk=;EntityPath=myeventhub");
 
-            #region Simulate Many
-
-            var random = new Random(Environment.TickCount);
-
-            for (var i = 0; i < 1000; i++)
-            {
-                var deviceTelemetry = DeviceTelemetry.GenerateRandom(random);
-                publisher.Publish(deviceTelemetry);
-            }
-
-            #endregion
-
-            #region Publish DeviceTelemetry
-
-            //var deviceTelemetry = new DeviceTelemetry
-            //{
-            //    DeviceType = DeviceType.Phone,
-            //    IpAddress = "127.0.0.1",
-            //    IsOn = true,
-            //    Time = DateTime.Now
-            //};
-
-            //publisher.Publish(deviceTelemetry);
-
-            #endregion
-
-            //var strings = new List<string> {"Hello", "World"};
-            //publisher.Publish(strings);
+            var strings = new List<string> {"Hello", "World"};
+            publisher.Publish(strings);
 
             var eventProcessorHostName = Guid.NewGuid().ToString();
             var eventProcessorHost = new EventProcessorHost(eventProcessorHostName, eventHubName,
