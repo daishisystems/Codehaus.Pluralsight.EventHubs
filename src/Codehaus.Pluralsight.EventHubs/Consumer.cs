372ab5ec520e9b71a27a9da6c4f8482fcf623ea8
Paul Mooney <paul@daishisystems.com>
2017-09-28 23:39:16 +0100

    Removed unnecessary regions.

--- src/Codehaus.Pluralsight.EventHubs/Consumer.cs
+++ src/Codehaus.Pluralsight.EventHubs/Consumer.cs
@@ -43,8 +43,6 @@ namespace Codehaus.Pluralsight.EventHubs
                 // 1. Convert the stream of bytes to string.
                 var data = Encoding.UTF8.GetString(eventData.GetBytes());
 
-                #region Deserialize device telemetry
-
                 // 2. Deserialize the object.
                 var deviceTelemetry = JsonConvert.DeserializeObject<DeviceTelemetry>(data);
                 Console.WriteLine("{0} published at {1:R}. Status: {2}",
@@ -52,10 +50,6 @@ namespace Codehaus.Pluralsight.EventHubs
                     deviceTelemetry.Time,
                     deviceTelemetry.IsOn ? "On" : "Off");
                 _counter++;
-
-                #endregion
-
-                // Console.WriteLine("Message received.  Partition: '{0}', Data: '{1}'", context.Lease.PartitionId, data);
             }
 
             if (_checkpointStopWatch.Elapsed > TimeSpan.FromMinutes(5))
