b684c464d726d3881d79eecebdb8d3acf8a6b6b8
Paul Mooney <paul@daishisystems.com>
2017-10-11 10:35:40 +0100

    Added new Event Hub connection metadata.

--- src/Codehaus.Pluralsight.EventHubs.PublisherApp/Program.cs
+++ src/Codehaus.Pluralsight.EventHubs.PublisherApp/Program.cs
@@ -9,7 +9,7 @@ namespace Codehaus.Pluralsight.EventHubs.PublisherApp
             var publisher = new Publisher();
 
             publisher.Init(
-                "Endpoint=sb://mooney.servicebus.windows.net/;SharedAccessKeyName=Publish;SharedAccessKey=YF3Eu+QUmfuxBXhlWvibaKRPVfgKvGr9eo25FId3nIk=;EntityPath=myeventhub");
+                "Endpoint=sb://simple-example.servicebus.windows.net/;SharedAccessKeyName=Publisher;SharedAccessKey=WFF0D3kZAme2nTeG7Nun2oc/YQIkX280pwbULWAF1OM=;EntityPath=simple-example");
 
             var random = new Random(Environment.TickCount);
             const int numEvents = 1000;
@@ -20,6 +20,7 @@ namespace Codehaus.Pluralsight.EventHubs.PublisherApp
             {
                 var deviceTelemetry = DeviceTelemetry.GenerateRandom(random);
                 publisher.Publish(deviceTelemetry);
+                Console.Clear();
                 Console.WriteLine($"Published {i + 1} events...");
             }
             Console.ReadLine();
